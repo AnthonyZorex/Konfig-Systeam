2f509b105f345f562b98400b8d90253d2a6da124
AnthonyZorex <bonettaanthony466@gmail.com>
2024-06-06 13:00:43 +0200

    fix 0.0.3

--- schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Release/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
+++ schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Release/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763feeea7d203da15e060fbd6714b4e3472b186f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca03f3af11325128a2456bcebdb8b646371aa62")]
 [assembly: System.Reflection.AssemblyProductAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
