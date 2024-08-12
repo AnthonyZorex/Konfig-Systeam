e12dfda42426baae7e69accbfca85e711427b6ff
AnthonyZorex <bonettaanthony466@gmail.com>
2024-08-12 16:09:21 +0200

    Bild 1.0.13

--- schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Release/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
+++ schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Release/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd8bee1a8ac9324b2fd54eb736304f359c7399b")]
 [assembly: System.Reflection.AssemblyProductAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
