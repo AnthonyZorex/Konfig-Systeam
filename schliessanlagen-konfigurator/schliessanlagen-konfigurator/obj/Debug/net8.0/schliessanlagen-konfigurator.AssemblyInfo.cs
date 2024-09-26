ce7b129e1f76fb4df23148e880f8b2c0f6f8686a
malyaroslav <malyi.yaroslav@gmail.com>
2024-09-26 12:39:50 +0200

    analitic

--- schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Debug/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
+++ schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Debug/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed9145bb1cec287269ddc073cb7077727501194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8689ee1eb3dcced51348ae424db8633d10128ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
