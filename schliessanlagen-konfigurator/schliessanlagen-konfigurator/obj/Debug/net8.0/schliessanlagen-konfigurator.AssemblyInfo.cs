ad42b55cdd2f855bf317d36e97dc7a27e72babfa
malyaroslav <malyi.yaroslav@gmail.com>
2024-10-22 15:32:20 +0200

    Update 2.11

--- schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Debug/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
+++ schliessanlagen-konfigurator/schliessanlagen-konfigurator/obj/Debug/net8.0/schliessanlagen-konfigurator.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a797b4957e5d261b6045050c4e7f4a12adc08ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5071476d5024cd1023d85738d4b2a376359984fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schliessanlagen-konfigurator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
